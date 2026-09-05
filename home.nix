adcd0c7e38e2284c059cac924f3ce54db1a58cb9
knowttl <knowttl42@gmail.com>
2026-09-05 10:20:27 -0700

    Add Pi Meta OAuth extension

--- home.nix
+++ home.nix
@@ -252,6 +252,8 @@ in
     config.lib.file.mkOutOfStoreSymlink "${dotfiles}/home/.pi/agent/keybindings.json";
   home.file.".pi/agent/models.json".source =
     config.lib.file.mkOutOfStoreSymlink "${dotfiles}/home/.pi/agent/models.json";
+  home.file.".pi/agent/settings.json".source =
+    config.lib.file.mkOutOfStoreSymlink "${dotfiles}/home/.pi/agent/settings.json";
   home.file.".pi/agent/extensions/terminal-status-title.js".source =
     config.lib.file.mkOutOfStoreSymlink "${dotfiles}/home/.pi/agent/extensions/terminal-status-title.js";
   home.file.".tmux.conf".source =
